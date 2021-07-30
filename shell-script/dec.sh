63c6e4fc02daa1f8ac5bf5dd3a1efef964334cfb
paulpeng-popo <kmes1234@gmail.com>
2021-07-31 00:40:04 +0800

    add file in/out args in both enc.sh and dec.sh

--- shell-script/dec.sh
+++ shell-script/dec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl rsautl -decrypt -inkey key.pem
+openssl rsautl -decrypt -inkey key.pem -in $1 -out $2
--- shell-script/enc.sh
+++ shell-script/enc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl rsautl -encrypt -pubin -inkey key.pub
+openssl rsautl -encrypt -pubin -inkey key.pub -in $1 -out $2
